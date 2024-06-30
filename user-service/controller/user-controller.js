d43ace23c90effafcd2d68d0c3019133ba608163
Bryan Matthew <bryanmatthew206@gmail.com>
2024-06-30 21:41:55 +0700

    feat: add login and register

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -1,11 +1,30 @@
 
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 class UserController {
     static async login(req, res, next) {
         try {
+            const { email, password } = req.body;
+            const user = await prisma.user.findUnique({
+                where: {
+                    email,
+                },
+            })
 
+            if (!user) {
+                throw { name: "UserNotFound" }
+            }
+
+            const match = await bcrypt.compare(password, user.password)
+            if (match) {
+                const token = jwt.sign({ email, password }, process.env.JWT_SECRET);
+                res.status(200).json({ message: "login successful", token })
+            } else {
+                throw { name: "InvalidCredentials" }
+            }
         } catch (err) {
             next(err);
         }
@@ -13,7 +32,17 @@ class UserController {
 
     static async register(req, res, next) {
         try {
-            console.log(req.body);
+            const { name, email, password } = req.body;
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+            await prisma.user.create({
+                data: {
+                    name,
+                    email,
+                    password: hash
+                }
+            })
+            res.status(200).json({ message: "register successful" });
         } catch (err) {
             next(err);
         }
