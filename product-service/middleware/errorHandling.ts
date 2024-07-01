a111688b45cff4ba210da8cd76bd1b7f2f52759f
Bryan Matthew <bryanmatthew206@gmail.com>
2024-07-01 21:37:20 +0700

    feat: add error handling middleware

--- product-service/middleware/errorHandling.ts
+++ product-service/middleware/errorHandling.ts
@@ -8,15 +8,6 @@ const errorHandler = (
 ) => {
     let message = "Internal server error";
     let code = 500;
-
-    if (err.name === "InvalidCredentials") {
-        message = "Incorrect username/password";
-        code = 400;
-    } else if (err.name === "UserNotFound") {
-        message = "User has not been registered";
-        code = 400;
-    }
-
     res.status(code).json({ message });
 };
 
